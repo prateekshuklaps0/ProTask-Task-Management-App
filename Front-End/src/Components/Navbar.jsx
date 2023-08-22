960162047a46a5c744420dd387471427ac58da35
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-08-22 20:30:50 +0530

    Update

--- Front-End/src/Components/Navbar.jsx
+++ Front-End/src/Components/Navbar.jsx
@@ -1,9 +1,17 @@
 import * as css from "../Styles/NavbarCss";
 import { useEffect, useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Image, Center, Link } from "@chakra-ui/react";
+
+import Logo from "../Images/Logo.png";
 
 const Navbar = () => {
-  return <Box css={css.OuterBox}>Navbar</Box>;
+  return (
+    <Box css={css.OuterBox}>
+      <Box css={css.TopInnerCont}>
+        <Image src={Logo} css={css.ImageCss} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Navbar;
--- Front-End/src/Styles/NavbarCss.js
+++ Front-End/src/Styles/NavbarCss.js
@@ -2,6 +2,37 @@ import { css } from "@emotion/react";
 
 export const OuterBox = css`
   width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+  background-color: white;
+
+  @media (max-width: 992px) {
+  }
+  @media (max-width: 768px) {
+  }
+  @media (max-width: 480px) {
+  }
+`;
+export const TopInnerCont = css`
+  width: 85%;
+  height: 60px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: 992px) {
+  }
+  @media (max-width: 768px) {
+  }
+  @media (max-width: 480px) {
+  }
+`;
+export const ImageCss = css`
+  width: 100px;
+  height: 100%;
+  border: 1px solid red;
 
   @media (max-width: 992px) {
   }
