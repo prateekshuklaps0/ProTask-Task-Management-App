2c21fc16eb3824a8aea0cb2fe784d17a857b1a8d
masaiharshit <harshit007.ara@gmail.com>
2023-08-25 00:42:30 +0530

    login

--- Front-End/src/Pages/Login.jsx
+++ Front-End/src/Pages/Login.jsx
@@ -1,7 +1,136 @@
-import React from 'react'
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Link,
+  Text,
+  useColorModeValue,
+  InputRightElement,
+  InputGroup,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate=useNavigate()
+
+  const submitLogin = () => {
+    const payload = {
+      email,
+      password,
+    };
+
+    axios.post(`${process.env.REACT_APP_SERVER}/users/login`, payload)
+      .then((res) =>{
+         alert(res.data.msg);
+         localStorage.setItem("frontendtoken",res.data.token)
+         navigate("/")
+        })
+      .catch((err) => console.log(err));
+
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <Flex
+      minH={"70vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack
+        //border={"1px solid red"}
+        w={"1000px"}
+        spacing={8}
+        mx={"auto"}
+        maxW={"lg"}
+        py={12}
+        px={6}
+      >
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign In </Heading>
+        </Stack>
+
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormControl>
+
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement h={"full"}>
+                  <Button variant={"ghost"} onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link color={"blue.400"}>Forgot password?</Link>
+              </Stack>
+
+              <Button
+                onClick={submitLogin}
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Sign in
+              </Button>
+            </Stack>
+
+            <Stack pt={6}>
+              <Text align={"center"}>
+                If have no account?{" "}
+                <NavLink style={{ color: "dodgerblue" }} to="/signup">
+                  Sign Up
+                </NavLink>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
