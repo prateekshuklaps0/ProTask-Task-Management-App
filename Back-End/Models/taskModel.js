ccb95c81a2c2951236949ba78480f0d0d3a8798f
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-08-27 22:21:51 +0530

    Did Some changes in task model

--- Back-End/Models/taskModel.js
+++ Back-End/Models/taskModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const TaskSchema = mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: String,
   status: { type: String, required: true },
   dueDate: { type: String, required: true },
   assignedTo: { type: String },
